a6d5a716f349f55835bde1873e2ad06d8add54d1
Myles Zhang <hanwenks@gmail.com>
2026-07-22 13:48:52 -0500

    End of L15 Working

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products, getProduct} from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 const today = dayjs();
 const deliveryDate = today.add(7, "days");
@@ -83,6 +84,19 @@ export function renderOrderSummary() {
 
     document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
 
+
+    document.querySelectorAll('.js-delete-link').forEach((link) => {
+        link.addEventListener('click', () => {
+        const productId = link.dataset.productId;
+        removeFromCart(productId);
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
+
+        renderPaymentSummary();
+        });
+    });
+
+
     document.querySelectorAll(".js-update-link").forEach((link) => {
     link.addEventListener("click", () => {
         const productId = link.dataset.productId;
@@ -119,6 +133,8 @@ export function renderOrderSummary() {
         );
         quantityLabel.innerHTML = newQuantity;
         container.classList.remove("is-editing-quantity");
+
+        renderPaymentSummary();
     });
     });
 
@@ -127,6 +143,7 @@ export function renderOrderSummary() {
         const { productId, deliveryOptionId } = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
     });
     });
 }
