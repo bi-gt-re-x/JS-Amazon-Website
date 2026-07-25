5409f67481751c499393c136e8bb7575fda41a00
Myles Zhang <hanwenks@gmail.com>
2026-07-25 12:52:05 -0500

    Promise and resolve in checkout Page

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,14 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
 
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    })
+}).then(() => {
+    
+})
+
 loadProducts(() => {
     renderOrderSummary();
     renderPaymentSummary();
