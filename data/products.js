aac0059ef6892d1a18a91c3ace1f0d5787397bdb
Myles Zhang <hanwenks@gmail.com>
2026-07-26 11:56:37 -0500

    Added Product Fetch

--- data/products.js
+++ data/products.js
@@ -73,6 +73,26 @@ export function getProduct(productId) {
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch('https://supersimplebackend.dev/products').then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+        let product;
+
+        if (productDetails.type === 'clothing') {
+            return new Clothing(productDetails);
+        } else if (productDetails.type === 'appliance') {
+            return new Appliance(productDetails);
+        } else {
+            return new Product(productDetails);
+        }
+    });;
+  });
+
+  return promise;
+}
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,16 +1,17 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
-import { loadProducts } from '../data/products.js';
+import { loadProducts, loadProductsFetch } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
-new Promise((resolve) => {
-    loadProducts(() => {
-        resolve();
+Promise.all([
+    loadProductsFetch(),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
     })
-}).then(() => {
     
-})
-
-loadProducts(() => {
+]).then(() => {
     renderOrderSummary();
     renderPaymentSummary();
 });
